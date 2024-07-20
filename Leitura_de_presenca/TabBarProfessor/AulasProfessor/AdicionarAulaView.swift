747fba66b19e51e47717304bcae8974fb7d107d6
joao-gabriel-cruz <joaogabrielpinhodaxruz@gmail.com>
2024-07-20 16:56:37 -0300

    list classroom teacher

--- Leitura_de_presenca/TabBarProfessor/AulasProfessor/AdicionarAulaView.swift
+++ Leitura_de_presenca/TabBarProfessor/AulasProfessor/AdicionarAulaView.swift
@@ -5,11 +5,43 @@
 //  Created by Mohamad Lobo on 19/07/24.
 //
 
+struct ClassRoom: Hashable {
+    var id: String;
+    var code: String;
+    var created_at: String
+    var lesson_name: String
+}
+
+
+
 import SwiftUI
 
+
+
 struct AdicionarAulaView: View {
+    @State var classrooms: [ClassRoom] = []
+    
+    func get_class_room(){
+        getClassromAPI(token: "token 6b68a2f1f5348c141c37cecd8b7f4831776bd92a"){ [self]rest in
+           classrooms = rest
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Listagem de aulas")
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .bold()
+        ScrollView {
+            ForEach(classrooms, id: \.self) { item in
+                ClassroumCardUIView(
+                    class_name: item.lesson_name,
+                    class_number: item.id,
+                    class_date: item.created_at
+                )
+            }
+        }.onAppear {
+            get_class_room()
+        }
     }
 }
 
--- Leitura_de_presenca/TabBarProfessor/AulasProfessor/ComponetsAdicionarAula/ClassroumCardUIView.swift
+++ Leitura_de_presenca/TabBarProfessor/AulasProfessor/ComponetsAdicionarAula/ClassroumCardUIView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct ClassroumCardUIView: View {
+    var class_name: String;
+    var class_number: String
+    var class_date: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 8)
+            .foregroundColor(.green)
+        .overlay {
+            HStack {
+                Image(systemName: "graduationcap.fill")
+                VStack(alignment: .leading) {
+                    Text(class_number + " Aula " + class_name)
+                        .fontWeight(.semibold)
+                    Text(class_date)
+                        .italic()
+                        .font(.subheadline)
+                }
+                Spacer()
+                Image(systemName: "")
+            }
+            .padding()
+            .foregroundColor(.white)
+        }
+        .frame(width: 350, height: 60)
     }
 }
 
 #Preview {
-    ClassroumCardUIView()
+    ClassroumCardUIView(
+        class_name: "Turma IOS",
+        class_number: "1",
+        class_date: "12/05/192"
+    )
 }
