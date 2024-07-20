2276cde2cce18d0fe412d565b2da4bac5766b877
joao-gabriel-cruz <joaogabrielpinhodaxruz@gmail.com>
2024-07-20 16:20:05 -0300

    alteração

---
+++ Leitura_de_presenca/TabBarProfessor/AulasProfessor/ComponetsAdicionarAula/ClassroumCardUIView.swift
@@ -0,0 +1,18 @@
+//
+//  ClassroumCardUIView.swift
+//  Leitura_de_presenca
+//
+//  Created by 08-L05-D-054875 on 20/07/24.
+//
+
+import SwiftUI
+
+struct ClassroumCardUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClassroumCardUIView()
+}
