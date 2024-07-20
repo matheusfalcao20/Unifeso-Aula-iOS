739e051bdc6bc8e5d22c8c51b40b3271f5104569
gusta&joao <gusta@email.com>
2024-07-20 10:53:31 -0300

    feat: class card

--- Leitura_de_presenca/TabBarAluno/Aula/AulaView.swift
+++ Leitura_de_presenca/TabBarAluno/Aula/AulaView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct AulaView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Suas aulas")
+                .font(.title2)
+                .fontWeight(.semibold)
+            Spacer()
+            ScrollView {}
+        }
+        .padding()
     }
 }
 
---
+++ Leitura_de_presenca/TabBarAluno/Aula/Components/ClassCard.swift
@@ -0,0 +1,48 @@
+//
+//  ProfileTabsView.swift
+//  login-screen
+//
+//  Created by 08-L05-D-054876 on 14/06/24.
+//
+
+import SwiftUI
+
+struct ClassCard: View {
+    var className: String
+    var classTime: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 8)
+            .foregroundColor(.gray)
+        .overlay {
+            HStack {
+                Image(systemName: "graduationcap.fill")
+                VStack(alignment: .leading) {
+                    Text(className)
+                        .fontWeight(.semibold)
+                    Text(classTime)
+                        .italic()
+                        .font(.subheadline)
+                }
+                Spacer()
+            }
+            .padding()
+            .foregroundColor(.white)
+        }
+        
+       
+        
+    }
+}
+
+struct ProfileTabsView_Preview: PreviewProvider {
+    
+    static var previews: some View {
+        ClassCard(
+            className: "Banco de dados", classTime: "19:00 - 21/10/2023"
+        )
+            .previewLayout(.fixed(width: 500, height: 100))
+            .padding()
+    }
+}
+
